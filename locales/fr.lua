903a0f699c58071419ef732c0dfb1833fa8ddf82
Kevin <34973659+keketiger@users.noreply.github.com>
2021-01-16 06:09:01 +0100

    Fix

--- locales/fr.lua
+++ locales/fr.lua
@@ -60,7 +60,7 @@ Locales['fr'] = {
   -- Service
   ['service_max'] = 'vous ne pouvez pas entrer en service, officiers en service: %s/%s',
   ['service_not'] = 'vous n\'êtes pas en service! Vous devez d\'abord enfiler votre tenue.',
-  ['service_anonunce'] = 'prise de service',
+  ['service_announce'] = 'prise de service',
   ['service_in'] = 'vous êtes en service, bon courage!',
   ['service_in_announce'] = 'l\'officier ~y~%s~s~ est entré en service!',
   ['service_out'] = 'vous avez terminé votre service.',
